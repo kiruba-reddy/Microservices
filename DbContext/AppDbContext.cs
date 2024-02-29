50d1a964187cfbde1520629f387defa312b3081f
Ubuntu <ubuntu@ip-172-31-15-105.ap-southeast-2.compute.internal>
2024-02-29 05:22:17 +0000

    project created in Linx

---
+++ DbContext/AppDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Trail.Models;
+
+namespace Trail.DbContexts;
+
+public class AppDbContext:DbContext
+{
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+        public DbSet<users> users { get; set; }
+	public DbSet<Address> Address { get; set; }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+        }
+}
---
+++ ModelConfiguration/AddressConfiguration.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using UserLogin.Models;
+
+namespace Trail.ModelConfiguration;
+
+public class AddressConfiguration : IEntityTypeConfiguration<Address>
+{
+	public void Configure(EntityTypeBuilder<Address> builder)
+	{
+	     builder.ToTable("e_address");
+	     builder.HasOne(u => u.user)
+	            .WithOne(a=>a.Address)
+	            .HasForeignKey<Address>(a => a.UserId);
+	     builder.HasKey(a => a.AddressId);
+	     builder.Property(a => a.City)
+	            .IsRequired()
+	            .HasMaxLength(50);
+	     builder.Property(a => a.Country)
+	            .IsRequired()
+	            .HasMaxLength(50);
+	     builder.Property(a=>a.PostalCode) 
+	            .IsRequired()
+	            .HasMaxLength(50);
+	  }
+}
